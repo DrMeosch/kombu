68cadd1f95b47f8f2dd142e63bef1d25e7f9015b
DrMeosch <51243387+DrMeosch@users.noreply.github.com>
2023-03-29 21:42:18 +0200

    fix

--- kombu/transport/azureservicebus.py
+++ kombu/transport/azureservicebus.py
@@ -264,7 +264,7 @@ class Channel(virtual.Channel):
 
         # message.body is either byte or generator[bytes]
         message = messages[0]
-        renewer.register(queue_obj.receiver, messages, max_lock_renewal_duration=60)
+        renewer.register(queue_obj.receiver, message, max_lock_renewal_duration=60)
 
         if not isinstance(message.body, bytes):
             body = b''.join(message.body)
